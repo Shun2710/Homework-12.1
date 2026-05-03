7e85a69fe1dfd0f63715e011fabb7b456c18a840
Shun2710 <alexandr.prokoptsov@gmail.com>
2026-05-03 23:21:24 +0100

    Add files via upload

---
+++ homework12.1.py
@@ -0,0 +1,13 @@
+# Task 12.1
+
+def prime_generator(n):
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield num
+
+print(list(prime_generator(10)))
